126b82b55c31584f1b2756ca875209e3dfc885f7
Zach Heiner <cardpunk1206@yahoo.com>
2019-02-15 12:31:39 -0700

    added stuff

--- csv-converter.js
+++ csv-converter.js
@@ -3,17 +3,17 @@ var dsv = require('d3-dsv');
 const fs = require('fs');
 const stripBOM = require('strip-bom');
 
-var d2lCSV = process.argv[2];
+var d2lCSV = './josh.csv';
 
-var courseId = '27206';
+var courseId = '80';
 
 var studentData = dsv.csvParse(stripBOM(fs.readFileSync(d2lCSV, 'utf8')));
 
 async function main(){
     try{
         var students = await canvas(courseId);
-
         console.log(students);
+        console.log(studentData);
     } catch(e){
         console.log(e);
     }
