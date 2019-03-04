1a6dcb4e97a924dc08bddd05d4d1328d518825cf
Seth Bolander <bol15015@byui.edu>
2019-03-04 15:56:23 -0700

    began mapping d2l student obj to canvas student obj

--- converter.js
+++ converter.js
@@ -1,15 +1,32 @@
-function getPointsPossible(csvStudent) {
-    var keynames = Object.keys(csvStudent);
-    var regex = /^([\s\S]*?) <[\s\S]*?MaxPoints:(\d+)/;
-    var points = keynames.map((keyName) => {
-        return regex.exec(keyName);
+function getAssignmentName(student) {
+    var keynames = Object.keys(student);
+    var assigns = keynames.filter(key => {
+        if (key !== 'OrgDefinedId' && key !== 'Username' && key !== 'Last Name' && key !== 'First Name' && key !== 'End-of-Line Indicator') {
+            return true;
+        } else {
+            return false;
+        }
+    }).map(key => {
+        return key.slice(0, -14);
     });
-    console.log(points);
+
+    return assigns;
 }
 
 module.exports = function convertAll(csvData, students) {
-    var newCSV = csvData;
-    var pointsPossible = getPointsPossible(csvData[0]);
 
-    return newCSV;
+    var newCSVData = csvData.map((student, i) => {
+        var newStudent = {};
+        newStudent['SIS User ID'] = student.OrgDefinedId.slice(1);
+        newStudent['SIS Login ID'] = student.Username.slice(1);
+        newStudent.Student = student['First Name'].concat(' ', student['Last Name']);
+        newStudent['Root Account'] = 'byui.instructure.com';
+        delete student['End-of-Line Indicator'];
+        var assignments = getAssignmentName(student);
+        console.log(assignments);
+
+        return newStudent;
+    });
+
+    return newCSVData;
 }
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var dsv = require('d3-dsv');
 const fs = require('fs');
 const stripBOM = require('strip-bom');
 
-var d2lCSV = './FDREL211.csv';
+var d2lCSV = './csv/Early ChildhoodSpecial Education - Major_GradesExport_2019-03-04-16-55.csv';
 
 var courseId = '80';
 
