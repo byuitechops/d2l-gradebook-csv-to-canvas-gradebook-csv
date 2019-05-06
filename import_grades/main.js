490caaf2a720781c9cb00c35c199c7adf47cc4c3
Seth Bolander <bol15015@byui.edu>
2019-05-06 17:07:46 -0600

    stuff

--- import_grades/main.js
+++ import_grades/main.js
@@ -1,6 +1,11 @@
 const canvas = require('canvas-api-wrapper');
 const puppeteer = require('puppeteer');
+const browser = require('puppeteer-canvas-login');
 let uploadButton = "#gradebook_upload_uploaded_data";
+
+async function login(input) {
+    await browser.login(input);
+}
 
 async function getGrades(course) {
     let hi = await canvas.get(`/api/v1/courses/${course}/users?enrollment_state%5B%5D=active&enrollment_state%5B%5D=invited&enrollment_type%5B%5D=student&enrollment_type%5B%5D=student_view&include%5B%5D=avatar_url&include%5B%5D=group_ids&include%5B%5D=enrollments&per_page=100`);
