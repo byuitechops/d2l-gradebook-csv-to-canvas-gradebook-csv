264800482dd8f013d2633c09387ed49c7c138aca
Seth Bolander <bol15015@byui.edu>
2019-05-06 16:51:56 -0600

    added import_grades folder

---
+++ import_grades/main.js
@@ -0,0 +1,17 @@
+const canvas = require('canvas-api-wrapper');
+const puppeteer = require('puppeteer');
+let uploadButton = "#gradebook_upload_uploaded_data";
+
+async function getGrades(course) {
+    let hi = await canvas.get(`/api/v1/courses/${course}/users?enrollment_state%5B%5D=active&enrollment_state%5B%5D=invited&enrollment_type%5B%5D=student&enrollment_type%5B%5D=student_view&include%5B%5D=avatar_url&include%5B%5D=group_ids&include%5B%5D=enrollments&per_page=100`);
+    console.dir(hi, { depth: null });
+}
+
+
+function input() {
+
+}
+
+function main() {
+
+}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Location of D2L gradebook csv export you want to convert
-var d2lCSV = './create_new_csv/output/Early ChildhoodSpecial Education - Major_GradesExport_2019-03-11-17-42.csv';
+// var d2lCSV = './create_new_csv/output/Early ChildhoodSpecial Education - Major_GradesExport_2019-03-11-17-42.csv';
 // var d2lCSV = './create_new_csv/output/Elementary Education - Major_GradesExport_2019-03-11-17-42.csv';
-// var d2lCSV = './create_new_csv/output/Secondary Education - Major_GradesExport_2019-03-11-17-42.csv';
+var d2lCSV = './create_new_csv/output/Secondary Education - Major_GradesExport_2019-03-11-17-42.csv';
 
 // Location of Email List csv and boolean to use it or not
 const useList = false;
@@ -22,9 +22,9 @@ if (useList) {
 }
 
 // canvas gradebook csv output file location
-const output = './output/earlychild/Early ChildhoodSpecial Education - Major_GradesImport';
+// const output = './output/earlychild/Early ChildhoodSpecial Education - Major_GradesImport';
 // const output = './output/elementary/Elementary Education - Major_GradesImport';
-// const output = './output/secondary/Secondary Education - Major_GradesImport';
+const output = './output/secondary/Secondary Education - Major_GradesImport';
 
 (async function main() {
     try {
@@ -32,12 +32,13 @@ const output = './output/earlychild/Early ChildhoodSpecial Education - Major_Gra
         var newCSVdata = convertCanvasStudentObjs(csvData, emailList, useList);
         // write file to output location
         var i = 0;
-        var num = '00';
+        var num = 0;
         do {
-            fs.writeFileSync(`${output}${num}.csv`, dsv.csvFormat(newCSVdata.slice(i, i + 100)));
-            console.log(chalk.green(`Wrote data set ${num} to ${output}${num}.csv`));
-            i += 100;
-            num = `0${i / 100}`;
+            let fileNum = `${num++}`.padStart(2, '0');
+            fs.writeFileSync(`${output}${fileNum}.csv`, dsv.csvFormat(newCSVdata.slice(i, i + 10)));
+            console.log(chalk.green(`Wrote data set ${fileNum} to ${output}${fileNum}.csv`));
+            i += 10;
+
         } while (i <= newCSVdata.length);
 
     } catch (e) {
